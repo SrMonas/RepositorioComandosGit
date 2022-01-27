db9c6d2bfa7e636a6c6c4d46f3bcc2ab7b13a057
Sergio <sergio2002salvador@gmail.com>
2022-01-27 22:45:42 +0100

    Probando

--- SergioSalvadorCuesta/PruebaPuntos.java
+++ SergioSalvadorCuesta/PruebaPuntos.java
@@ -13,44 +13,6 @@ import com.gf.poo.personas.Persona;
  */
 public class PruebaPuntos {
 
-    public static void main(String[] args) {
-
-        Punto p1 = new Punto();
-        p1.setX(10);
-        p1.setY(20);
-        System.out.println(p1.toString());
-
-        Punto p2 = new Punto(-5, 50);
-        System.out.println(p2.toString());
-
-        System.out.println("Movemos el punto P1...");
-        System.out.println("\tOriginalmente: " + p1.toString());
-        p1.mover(-8, -3.5);
-        System.out.println("\tDespués del movimiento: " + p1.toString());
-
-        System.out.println("Movemos el punto P2...");
-        System.out.println("\tOriginalmente: " + p2.toString());
-        p2.mover(p1);
-        System.out.println("\tDespués del movimiento: " + p2.toString());
-        
-        System.out.println("Desplazamos el punto P1...");
-        System.out.println("\tOriginalmente: " + p1.toString());
-        p1.desplazar(-1, 1);
-        System.out.println("\tDespués del desplazamiento: " + p1.toString());
-        
-        System.out.println("Calculamos la distancia entre los puntos...");
-        System.out.println("\t" + p1.toString());
-        System.out.println("\t" + p2.toString());
-        double distancia=p1.distancia(p2);
-        System.out.println("\tDistancia metodo dinámico: " + distancia);        
-        System.out.println("\tDistancia método estático: " + Punto.distancia(p1, p2));
-        
-        System.out.println("Movemos los puntos al centro de la pantalla...");
-        p1.alCentro();
-        p2.alCentro();
-        System.out.println("\t" + p1.toString());
-        System.out.println("\t" + p2.toString());       
-       
-    }
+    
 
 }
